e9a2160d2cb22d04d44830bbbceb06ea242621f6
Zidni Mubarock <zidmubarock@gmail.com>
2016-09-07 00:28:11 +0700

    v1

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 import express     from "express"
 import bodyParser  from "body-parser"
+import morgan from "morgan"
 import ServiceCharge from "./lib/services/charge_service"
 
 const app = express();
 const port = process.env.PORT || 3001
 
+app.use(morgan('short'))
 app.use(bodyParser.json());
 
 app.post('/api/v1/charge', (req, res)=>{
   ServiceCharge(req.body, (error, response) => {
-    if(error)
+    if(error){
+      consol.log(error)
       return res.send({status: 'error'}).status(400);
+    }
 
     res.send({status: 'success', transaction: response})
   })
